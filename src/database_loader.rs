5ac2e15132dbe5b4e129e50f1c40431d49a7f47a
replydev <replydev@protonmail.com>
2020-12-13 02:30:52 +0100

    First src

---
+++ src/database_loader.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::Read;
+use json;
+
+pub struct OTPElement {
+    secret: String,
+    label: String,
+    algorithm: String
+}
+
+impl OTPElement {
+    pub fn new(secret: String, label: String, algorithm: String) -> OTPElement {
+        OTPElement {
+            secret: secret,
+            algorithm: algorithm,
+            label: label
+        }
+    }
+    pub fn secret(&self) -> String {
+        self.secret.to_string()
+    }
+    pub fn label(&self) -> String{
+        self.label.to_string()
+    }
+    pub fn algorithm(&self) -> String{
+        self.algorithm.to_string()
+    }
+}
+
+pub fn read_from_file(filename: String) -> Vec<OTPElement> {
+    let mut elements: Vec<OTPElement> = Vec::new();
+    let mut file = File::open(filename).expect("File not found!");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let json_object = json::parse(&contents).unwrap();
+    for i in 0..json_object.len() {
+        elements.push(OTPElement{
+            secret: json_object[i]["secret"].to_string(),
+            label: json_object[i]["label"].to_string(),
+            algorithm: json_object[i]["algorithm"].to_string()
+        });
+    }
+    //rust close files at the end of the function
+    elements
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::env;
+use std::fs;
+//mod encryption;
+mod database_loader;
+extern crate directories;
+extern crate otp;
+use otp::make_totp;
+use directories::{BaseDirs, UserDirs, ProjectDirs};
 fn main() {
-    println!("Hello, world!");
+    println!("cotp - written by @replydev");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2{
+        if args[1] == "--import"{
+            import_database(&args[2]);
+        }
+        else{
+            println!("Help")
+        }
+    }
+    else{
+        let elements: Vec<database_loader::OTPElement> = database_loader::read_from_file(get_db_path());
+        for i in 0..elements.len() {
+
+            let secret : &str = &elements[i].secret();
+
+            println!("{}: {}",elements[i].label(),make_totp(secret,30, 0).unwrap());
+
+
+            //println!("{} - {} - {}", elements[i].secret(), elements[i].label(), elements[i].algorithm());
+        }
+    }
+}
+
+
+fn get_db_path() -> String{
+    let mut home_dir :String = "".to_string();
+
+    if let Some(base_dirs) = BaseDirs::new(){
+        if let Some(home) = base_dirs.home_dir().to_str() {
+            home_dir = home.to_string();
+        }
+    }
+
+    home_dir.push_str("/.cotp");
+
+    fs::create_dir_all(&home_dir).expect("Failed to create directory!");
+    home_dir.push_str("/db");
+    home_dir
+}
+
+fn import_database(filename: &String){
+
+    let home_dir = get_db_path();
+    
+    fs::copy(filename,&home_dir).expect("Failed to import database");
+    println!("Successfully imported database");
 }
