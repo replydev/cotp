54161f0bf5ca0be878be5d6bf7c30b5b75b4c083
replydev <replydev@protonmail.com>
2020-12-19 00:24:33 +0100

    String encryption

--- src/cryptograpy.rs
+++ src/cryptograpy.rs
@@ -1,13 +1,10 @@
 use std::{error, fmt};
-use std::fs::File;
-use std::io::prelude::*;
 use sodiumoxide::crypto::pwhash;
-use sodiumoxide::crypto::secretstream::
-    {Stream, Tag, ABYTES, HEADERBYTES, KEYBYTES};
+use sodiumoxide::crypto::secretstream::{Stream, Tag, KEYBYTES};
 use sodiumoxide::crypto::secretstream::xchacha20poly1305::{Header, Key};
 
 const CHUNKSIZE: usize = 4096;
-const SIGNATURE: [u8; 4] = [0xC1, 0x0A, 0x4B, 0xED];
+const SIGNATURE: [u8;4] = [0xC1, 0x0A, 0x4B, 0xED];
 
 #[derive(Debug)]
 struct CoreError {
@@ -26,74 +23,40 @@ impl fmt::Display for CoreError {
 
 impl error::Error for CoreError {}
 
-pub fn encrypt(in_file: &mut File, out_file: &mut File, password: &str) 
-    -> Result<(), Box<dyn error::Error>> {
-
-    // change to vec to allocate on heap and have larger chunksize
-    // was overflowing stack when set to 1_000_000
-    let mut buf = [0; CHUNKSIZE];
-    let mut bytes_left = in_file.metadata()?.len();
-    
-    // write file signature
-    out_file.write(&SIGNATURE)?;
-
+pub fn encrypt_string(plaintext: &mut String,password: &str) -> String {
+    let mut encrypted = String::new();
+    encrypted.push_str(&base64::encode(SIGNATURE));
+    encrypted.push('|');
     let salt = pwhash::gen_salt();
-    out_file.write(&salt.0)?;
-
+    encrypted.push_str(&base64::encode(salt.0));
+    encrypted.push('|');
     let mut key = [0u8; KEYBYTES];
     pwhash::derive_key(&mut key, password.as_bytes(), &salt,
         pwhash::OPSLIMIT_INTERACTIVE,
         pwhash::MEMLIMIT_INTERACTIVE).unwrap();
     let key = Key(key);
-    let (mut stream, header) = Stream::init_push(&key)
-        .map_err(|_| CoreError::new("init_push failed"))?;
-    out_file.write(&header.0)?;
 
-    loop {
-        match (*in_file).read(&mut buf) {
-            Ok(num_read) if num_read > 0 => {
-                bytes_left -= num_read as u64;
-                let tag = match bytes_left {
-                    0 => Tag::Final,
-                    _ => Tag::Message,
-                };
-                out_file.write(
-                    &stream.push(&buf[..num_read], None, tag)
-                        .map_err(|_| CoreError::new("Encrypting file failed"))?
-                )?;
-                continue
-            },
-            Err(e) => Err(e)?,
-            _ => break, // reached EOF
-        }
-    }
+    let (mut enc_stream, header) = Stream::init_push(&key).unwrap();
 
-    Ok(())
+    encrypted.push_str(&base64::encode(header.0));
+    encrypted.push('|');
+
+    let encrypted_string = enc_stream.push(plaintext.as_bytes(), None, Tag::Message).expect("Cannot encrypt");
+
+    encrypted.push_str(&base64::encode(encrypted_string));
+    encrypted
 }
 
-pub fn decrypt(in_file: &mut File, password: &str)
-    -> Result<String, Box<dyn error::Error>> {
-
-    // make sure file is at least prefix + salt + header
-    if !(in_file.metadata()?.len() > (pwhash::SALTBYTES + HEADERBYTES + SIGNATURE.len()) as u64) {
-        return Err(CoreError::new("File not big enough to have been encrypted"))?;
-    }
-
-    let mut salt = [0u8; pwhash::SALTBYTES];
-    let mut signature = [0u8; 4];
-
-    in_file.read_exact(&mut signature)?;
-    if signature == SIGNATURE { // if the signature is present, read into all of salt
-        in_file.read_exact(&mut salt)?;
-    } else { // or take the bytes from signature and read the rest from file
-        &mut salt[..4].copy_from_slice(&signature);
-        in_file.read_exact(&mut salt[4..])?;
-    }
-    let salt = pwhash::Salt(salt);
-
-    let mut header = [0u8; HEADERBYTES];
-    in_file.read_exact(&mut header)?;
-    let header = Header(header);
+pub fn decrypt_string(encrypted_text: &mut str,password: &str) -> Result<String, Box<dyn error::Error>> {
+    let string_len = encrypted_text.len();
+    let split = encrypted_text.split('|');
+    let vec: Vec<&str> = split.collect();
+    let signature = base64::decode(vec[0]).unwrap();
+    let byte_salt = base64::decode(vec[1]).unwrap();
+    let salt = pwhash::Salt(byte_vec_to_byte_array(byte_salt));
+    let byte_header = base64::decode(vec[2]).unwrap();
+    let header = Header(header_vec_to_header_array(byte_header));
+    let cipher = base64::decode(vec[3]).unwrap();
 
     let mut key = [0u8; KEYBYTES];
     pwhash::derive_key(&mut key, password.as_bytes(), &salt,
@@ -102,25 +65,28 @@ pub fn decrypt(in_file: &mut File, password: &str)
         .map_err(|_| CoreError::new("Deriving key failed"))?;
     let key = Key(key);
 
-    let mut buffer = [0u8; CHUNKSIZE + ABYTES];
     let mut stream = Stream::init_pull(&header, &key)
         .map_err(|_| CoreError::new("init_pull failed"))?;
 
-    let mut contents = String::new();
+    let (decrypted, tag1) = stream.pull(&cipher, None).unwrap();
 
-    while stream.is_not_finalized() {
-        match in_file.read(&mut buffer) {
-            Ok(num_read) if num_read > 0 => {
-                let (decrypted, _tag) = stream.pull(&buffer[..num_read], None)
-                    .map_err(|_| CoreError::new("Incorrect password"))?;
-                contents.push_str(&String::from_utf8(decrypted).unwrap());
-                continue
-            },
-            Err(e) => return Err(Box::new(e)),
-            _ => return Err(CoreError::new("Decryption error"))?, // reached EOF
-        }
+    Ok(String::from_utf8(decrypted).unwrap())
+}
+
+fn byte_vec_to_byte_array(byte_vec: Vec<u8>) -> [u8;32]{
+    let mut array = [0u8;32];
+    for (place, element) in array.iter_mut().zip(byte_vec.iter()) {
+        *place = *element;
     }
-    Ok(contents)
+    array
+}
+
+fn header_vec_to_header_array(byte_vec: Vec<u8>) -> [u8;24]{
+    let mut array = [0u8;24];
+    for (place, element) in array.iter_mut().zip(byte_vec.iter()) {
+        *place = *element;
+    }
+    array
 }
 
 pub fn prompt_for_passwords(message: &str) -> String{
