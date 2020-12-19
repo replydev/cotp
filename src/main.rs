04c42c58e623a3dc29ac3c739dad1889fb631201
replydev <replydev@protonmail.com>
2020-12-19 23:21:06 +0100

    Version 0.0.6

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod utils;
 use utils::get_db_path;
 mod cryptograpy;
 fn main() {
-    let version = "0.0.5.1";
+    let version = "0.0.6";
     print_title(version);
     let args: Vec<String> = env::args().collect();
     if !args_parser(args){
