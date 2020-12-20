8d72c851499a8abbf77bd989789d329d61c8cfea
replydev <replydev@protonmail.com>
2020-12-20 12:54:36 +0100

    Init sodiumoxide

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod utils;
 mod argument_functions;
 mod otp_helper;
 mod cryptograpy;
+use sodiumoxide;
 
 #[cfg(debug_assertions)]
 fn print_title(version: &str){
@@ -19,9 +20,21 @@ fn print_title(version: &str){
     println!("written by @replydev\n");
 }
 
+fn init() -> Result<(), ()>{
+    sodiumoxide::init()
+}
+
 fn main() {
     let version = "0.0.6";
     print_title(version);
+    let init_result = init();
+    match init_result {
+        Ok(()) => println!("sodiumoxide init succeeded"),
+        Err(()) => { 
+            println!("Failed to init sodiumoxide");
+            return;
+        }
+    }
     let args: Vec<String> = env::args().collect();
     if !args_parser(args){
         utils::create_db_if_needed();
@@ -35,12 +48,12 @@ fn args_parser(args: Vec<String>) -> bool {
     }
 
     match &args[1][..]{
-        "-i"  | "--import" =>{argument_functions::import(args);},
-        "-h"  | "--help" =>{argument_functions::help();},
-        "-a"  | "--add" =>{argument_functions::add(args);},
-        "-r"  | "--remove" =>{argument_functions::remove(args);},
-        "-e"  | "--edit" =>{argument_functions::edit(args);},
-        "-ex" | "--export" =>{argument_functions::export(args);},
+        "-i"  | "--import" => argument_functions::import(args),
+        "-h"  | "--help" => argument_functions::help(),
+        "-a"  | "--add" => argument_functions::add(args),
+        "-r"  | "--remove" => argument_functions::remove(args),
+        "-e"  | "--edit" => argument_functions::edit(args),
+        "-ex" | "--export" => argument_functions::export(args),
         _=>{
             println!("Invalid argument: {}, type cotp -h to get command options", args[1]);
             return true;
