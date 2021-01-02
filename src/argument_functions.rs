b97eddc14ac74fddd305e0a5717bf78f0bea111e
replydev <replydev@protonmail.com>
2021-01-02 17:28:15 +0100

    Print compatible backups

--- src/argument_functions.rs
+++ src/argument_functions.rs
@@ -7,7 +7,7 @@ pub fn help(){
     println!("-a,--add <secret> <issuer> <label>       | Add a new OTP code");
     println!("-r,--remove <secret> <issuer> <label>    | Remove an OTP code");
     println!("-e,--edit <id> <secret> <issuer> <label> | Edit an OTP code");
-    println!("-i,--import <appname> <filename>         | Import a backup from a given application");
+    println!("-i,--import <appname> <path>             | Import a backup from a given application");
     println!("-ex,--export                             | Export the entire database in a plaintext json format");
     println!("-j,--json                                | Print results in json format");
     println!("-h,--help                                | Print this help");
@@ -38,7 +38,11 @@ pub fn import(args: Vec<String>){
         println!("Successfully imported database");
     }
     else{
-        println!("Invalid arguments, type cotp --import <backup_format> <path>");
+        println!("Invalid arguments, type cotp --import <appname> <path>");
+        println!("cotp can import backup from:");
+        println!("\"cotp\"");
+        println!("\"aegis\"");
+        println!("\"andotp\"");
     }
 }
 
