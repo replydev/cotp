c0be6a97d6afaea8e4f20f733ad0219f18e42bc4
replydev <replydev@protonmail.com>
2021-01-06 15:02:14 +0100

    Version 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn init() -> Result<(), ()>{
 }
 
 fn main() {
-    let version = "0.1.0";
+    let version = "0.1.1";
     print_title(version);
     let init_result = init();
     match init_result {
