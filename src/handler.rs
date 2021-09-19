55be4815fb09cc4a5af9028135f48ef10c43279a
replydev <commoncargo@tutanota.com>
2021-09-19 09:51:37 +0200

    Copy otp code to clipboard if user press enter

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,7 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 use crate::app::{App, AppResult};
+use copypasta::{ClipboardContext, ClipboardProvider};
 
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
@@ -20,12 +21,24 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
             app.running = false;
         }
 
+        // Move into the table
         KeyCode::Up => {
             app.table.previous();
         }
 
         KeyCode::Down => {
             app.table.next();
+        }
+
+        KeyCode::Enter => {
+            if let Some(selected) = app.table.state.selected(){
+                if let Some(element) = app.table.items.get(selected){
+                    if let Some(otp_code) = element.get(3){
+                        let mut ctx = ClipboardContext::new().unwrap();
+                        ctx.set_contents(otp_code.to_owned()).unwrap();
+                    }
+                }
+            }
         }
         _ => {}
     }
