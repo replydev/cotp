23b49518f2cf61be7c35b05bb230326208b6a510
replydev <commoncargo@tutanota.com>
2021-10-25 23:08:31 +0200

    Rewrite freeotp_plus secret encoding

--- src/importers/freeotp_plus.rs
+++ src/importers/freeotp_plus.rs
@@ -23,11 +23,10 @@ struct FreeOTPElement {
     #[serde(rename = "label")]
     _label: String,
     period: u64,
-    secret: Vec<i16>,
+    secret: Vec<i8>,
     #[serde(rename = "type")]
     _type: String,
 }
-
 
 pub fn import(file_path: &str) -> Result<Vec<OTPElement>,String>{
     let json = match read_to_string(file_path) {
@@ -45,7 +44,7 @@ pub fn import(file_path: &str) -> Result<Vec<OTPElement>,String>{
     for label in freeotp.token_order {
         elements.push(
             OTPElement::new(
-                i16_secret_to_base32(&freeotp.tokens[i].secret),
+                encode_secret(&freeotp.tokens[i].secret),
                 freeotp.tokens[i].issuer_ext.clone(),
                 label,
                 freeotp.tokens[i].digits,
@@ -64,27 +63,26 @@ pub fn import(file_path: &str) -> Result<Vec<OTPElement>,String>{
     Ok(elements)
 }
 
-
-fn i16_secret_to_base32(secret: &Vec<i16>) -> String {
-    let mut converted_vec: Vec<u8> = Vec::with_capacity(secret.len());
-    for i in secret {
-        converted_vec.push(*i.to_le_bytes().first().unwrap_or(&0u8));
-    }
-    BASE32_NOPAD.encode(&converted_vec.as_slice())
+fn encode_secret(secret: &Vec<i8>) -> String {
+    BASE32_NOPAD.encode(secret
+        .iter()
+        .map(|n| *n as u8)
+        .collect::<Vec<u8>>()
+        .as_slice()
+    )
 }
-
 
 #[cfg(test)]
 mod tests{
-    use super::i16_secret_to_base32;
+    use super::encode_secret;
 
     #[test]
     fn test_secret_conversion(){
-        let secret: Vec<i16> = 
+        let secret: Vec<i8> =
         vec![-34,104,-37,-33,82,-89,-93,-105,-14,5,100,-73,-84,1,11,73,101,-92
         ,106,122,-90,111,-119,30,87,-6,16,-57,-126,25,0,-65,-35,-76,-38];
 
-        assert_eq!(i16_secret_to_base32(&secret),String::from("3ZUNXX2SU6RZP4QFMS32YAILJFS2I2T2UZXYSHSX7IIMPAQZAC753NG2"));
+        assert_eq!(encode_secret(&secret), String::from("3ZUNXX2SU6RZP4QFMS32YAILJFS2I2T2UZXYSHSX7IIMPAQZAC753NG2"));
     }
 }
 
