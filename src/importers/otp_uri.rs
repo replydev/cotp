8a18b54669232bd484268b7c47c5b7674a5bc948
replydev <commoncargo@tutanota.com>
2023-09-15 18:59:09 +0200

    Implement try_from as requested from importer implementation

--- src/importers/otp_uri.rs
+++ src/importers/otp_uri.rs
@@ -1,18 +1,17 @@
 use crate::exporters::otp_uri::OtpUriList;
 use crate::otp::from_otp_uri::FromOtpUri;
-use crate::otp::otp_element::{OTPDatabase, OTPElement};
+use crate::otp::otp_element::OTPElement;
 
-impl From<OtpUriList> for OTPDatabase {
-    fn from(value: OtpUriList) -> Self {
-        let input_length = value.items.len();
-        let converted_elements = value
+impl TryFrom<OtpUriList> for Vec<OTPElement> {
+    type Error = String;
+
+    fn try_from(value: OtpUriList) -> Result<Self, Self::Error> {
+        Ok(value
             .items
             .into_iter()
             .map(|e| OTPElement::from_otp_uri(e.as_str()))
             .filter(|e| e.is_ok())
             .map(|e| e.unwrap())
-            .collect();
-
-        OTPDatabase::from(converted_elements)
+            .collect())
     }
 }
