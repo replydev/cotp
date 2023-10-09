b15e1df8d7c6c6f6e625836fc329b8a526f1af61
replydev <commoncargo@tutanota.com>
2023-10-09 21:27:27 +0200

    Compile wayland code only if the target is linux

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,5 +1,6 @@
 use base64::{engine::general_purpose, Engine as _};
 use copypasta_ext::prelude::*;
+#[cfg(target_os = "linux")]
 use copypasta_ext::wayland_bin::WaylandBinClipboardContext;
 use copypasta_ext::x11_bin::ClipboardContext as BinClipboardContext;
 use copypasta_ext::x11_fork::ClipboardContext as ForkClipboardContext;
@@ -34,12 +35,17 @@ fn ssh_clipboard(content: &str) -> bool {
         )
         .is_ok()
 }
-
+#[cfg(target_os = "linux")]
 fn wayland_clipboard(content: &str) -> bool {
     env_var_set("WAYLAND_DISPLAY")
         && WaylandBinClipboardContext::new()
             .and_then(|mut ctx| ctx.set_contents(content.to_owned()))
             .is_ok()
+}
+
+#[cfg(not(target_os = "linux"))]
+fn wayland_clipboard(content: &str) -> bool {
+    false
 }
 
 fn other_platform_clipboard(content: &str) -> bool {
