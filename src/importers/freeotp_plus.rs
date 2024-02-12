5a49c7dde5715ee97d451193395abcca7db76753
replydev <commoncargo@tutanota.com>
2024-02-12 23:59:53 +0100

    Add constructor for freeOTP plus model + expose fields + write integration test

--- src/importers/freeotp_plus.rs
+++ src/importers/freeotp_plus.rs
@@ -10,19 +10,31 @@ pub struct FreeOTPPlusJson {
     tokens: Vec<FreeOTPElement>,
 }
 
-#[derive(Serialize, Deserialize)]
-struct FreeOTPElement {
-    algo: String,
-    counter: u64,
-    digits: u64,
+impl FreeOTPPlusJson {
+    /// Creates a new instance of FreeOTPPlusJSON. Currently we clone the tokens label to retrieve the tokens order.
+    pub fn new(tokens: Vec<FreeOTPElement>) -> Self {
+        let token_order: Vec<String> = tokens.iter().map(|e| &e._label).cloned().collect();
+
+        Self {
+            token_order,
+            tokens,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct FreeOTPElement {
+    pub algo: String,
+    pub counter: u64,
+    pub digits: u64,
     #[serde(rename = "issuerExt")]
-    issuer_ext: String,
+    pub issuer_ext: String,
     #[serde(rename = "label")]
-    _label: String,
-    period: u64,
-    secret: Vec<i8>,
+    pub _label: String,
+    pub period: u64,
+    pub secret: Vec<i8>,
     #[serde(rename = "type")]
-    _type: String,
+    pub _type: String,
 }
 
 impl From<FreeOTPElement> for OTPElement {
@@ -68,9 +80,14 @@ mod tests {
     use std::path::PathBuf;
 
     use crate::{
-        importers::importer::import_from_path,
+        importers::{freeotp_plus::FreeOTPElement, importer::import_from_path},
         otp::{otp_algorithm::OTPAlgorithm, otp_element::OTPElement, otp_type::OTPType},
     };
+
+    use std::fs;
+
+    use crate::otp::otp_element::OTPDatabase;
+    use color_eyre::Result;
 
     use super::{encode_secret, FreeOTPPlusJson};
 
@@ -120,6 +137,54 @@ mod tests {
                 }
             ],
             imported.unwrap()
+        )
+    }
+
+    #[test]
+    fn test_freeotp_export() {
+        // Arrange
+        let input_json: String = fs::read_to_string(PathBuf::from("test_samples/cotp_input.json"))
+            .expect("Cannot read input file for test");
+        let input_cotp_database: OTPDatabase =
+            serde_json::from_str(input_json.as_str()).expect("Cannot deserialize into input JSON");
+
+        // Act
+        let converted: Result<FreeOTPPlusJson> = input_cotp_database.try_into();
+
+        // Assert
+        assert!(converted.is_ok());
+
+        let free_otp = converted.unwrap();
+
+        assert_eq!(
+            vec!["label1".to_string(), "label2".to_string()],
+            free_otp.token_order
+        );
+
+        assert_eq!(
+            vec![
+                FreeOTPElement {
+                    algo: "SHA1".to_string(),
+                    counter: 0,
+                    digits: 6,
+                    issuer_ext: String::default(),
+                    _label: "label1".to_string(),
+                    period: 30,
+                    secret: vec![0],
+                    _type: "TOTP".to_string()
+                },
+                FreeOTPElement {
+                    algo: "SHA256".to_string(),
+                    counter: 3,
+                    digits: 6,
+                    issuer_ext: "ciccio".to_string(),
+                    _label: "label2".to_string(),
+                    period: 30,
+                    secret: vec![0],
+                    _type: "HOTP".to_string()
+                }
+            ],
+            free_otp.tokens
         )
     }
 }
