ea2b91f72d9aec2e5b6e475ebe5a0c90dffe0563
replydev <commoncargo@tutanota.com>
2025-07-14 19:03:13 +0200

    test: add help integration test

--- tests/cli_integration_tests.rs
+++ tests/cli_integration_tests.rs
@@ -1,5 +1,5 @@
 use assert_cmd::Command;
-use predicates::str::is_match;
+use predicates::str::{is_empty, is_match, starts_with};
 
 #[test]
 fn test_version_subcommand() {
@@ -12,5 +12,22 @@ fn test_version_subcommand() {
     // Assert
     assertion
         .success()
-        .stdout(is_match("^cotp \\d+\\.\\d+\\.\\d+-DEBUG-.+").unwrap());
+        .stdout(is_match("^cotp \\d+\\.\\d+\\.\\d+-DEBUG-.+").unwrap())
+        .stderr(is_empty());
+}
+
+#[test]
+fn test_help_subcommand() {
+    // Arrange / Act
+    let assertion = Command::cargo_bin("cotp").unwrap().arg("--help").assert();
+
+    // Assert
+    assertion
+        .success()
+        .stdout(starts_with(
+            "Trustworthy, encrypted, command-line TOTP/HOTP authenticator app with import functionality.
+
+Usage: cotp [OPTIONS] [COMMAND]",
+        ))
+        .stderr(is_empty());
 }
