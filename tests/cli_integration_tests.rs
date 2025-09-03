a7a358c5dfdb537f19baf89bb6ac83d1504b91cc
replydev <commoncargo@tutanota.com>
2025-09-03 20:37:21 +0200

    chore: run tests only in non-windows environments

--- tests/cli_integration_tests.rs
+++ tests/cli_integration_tests.rs
@@ -1,28 +1,31 @@
-use assert_cmd::Command;
-use predicates::str::{is_empty, is_match, starts_with};
+#[cfg(not(target_os = "windows"))] // TODO, Integration tests currently does not work on Windows
+mod cli_integration_test {
 
-#[test]
-fn test_version_subcommand() {
-    // Arrange / Act
-    let assertion = Command::cargo_bin("cotp")
-        .unwrap()
-        .arg("--version")
-        .assert();
+    use assert_cmd::Command;
+    use predicates::str::{is_empty, is_match, starts_with};
 
-    // Assert
-    assertion
-        .success()
-        .stdout(is_match("^cotp \\d+\\.\\d+\\.\\d+-DEBUG-.+").unwrap())
-        .stderr(is_empty());
-}
+    #[test]
+    fn test_version_subcommand() {
+        // Arrange / Act
+        let assertion = Command::cargo_bin("cotp")
+            .unwrap()
+            .arg("--version")
+            .assert();
 
-#[test]
-fn test_help_subcommand() {
-    // Arrange / Act
-    let assertion = Command::cargo_bin("cotp").unwrap().arg("--help").assert();
+        // Assert
+        assertion
+            .success()
+            .stdout(is_match("^cotp \\d+\\.\\d+\\.\\d+-DEBUG-.+").unwrap())
+            .stderr(is_empty());
+    }
 
-    // Assert
-    assertion
+    #[test]
+    fn test_help_subcommand() {
+        // Arrange / Act
+        let assertion = Command::cargo_bin("cotp").unwrap().arg("--help").assert();
+
+        // Assert
+        assertion
         .success()
         .stdout(starts_with(
             "Trustworthy, encrypted, command-line TOTP/HOTP authenticator app with import functionality.
@@ -30,4 +33,5 @@ fn test_help_subcommand() {
 Usage: cotp [OPTIONS] [COMMAND]",
         ))
         .stderr(is_empty());
+    }
 }
